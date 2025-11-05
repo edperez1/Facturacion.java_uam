7bf63bad61b892fd13eaeba30ce8f9a0cbf866fc
Ervin Perez <165834695+edperez1@users.noreply.github.com>
2025-11-04 21:43:27 -0600

    Cree todas las clases

---
+++ src/main/java/org/jadg13/Fcturacion/modelo/Categoria.java
@@ -0,0 +1,18 @@
+package org.jadg13.Fcturacion.modelo;
+import  javax.persistence.*;
+import  org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity @Getter @Setter
+public class Categoria {
+    @Id
+    @Hidden
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    @Column(length=32)
+    String oid;
+
+    @Column(length=50)
+    String descripcion;
+}
---
+++ src/main/java/org/jadg13/Fcturacion/modelo/Producto.java
@@ -0,0 +1,4 @@
+package org.jadg13.Fcturacion.modelo;
+
+public class Producto {
+}
